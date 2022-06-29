6fc2256295cee9b6ada2501a009e494b8ee1f1d4
wogus4048 <67767276+wogus4048@users.noreply.github.com>
2022-06-29 11:14:43 +0900

    [Bronze III] Title: A+B - 8, Time: 168 ms, Memory: 14992 KB -BaekjoonHub

--- 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -11,19 +11,14 @@ public class Main{
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     int test_case = Integer.parseInt(br.readLine());
-    String[][]arr = new String[test_case][2];
-    for(int i=0;i<test_case;i++)
-    {
-      arr[i] = br.readLine().split(" ");
-    }
+
 
 
     for(int i=0;i<test_case;i++)
     {
-
-      int result = Integer.parseInt(arr[i][0])+Integer.parseInt(arr[i][1]);
-
-      System.out.println(String.format("Case #%d: %s + %s = %d",i+1,arr[i][0],arr[i][1],result ));
+      String[] arr = br.readLine().split(" ");
+      int result = Integer.parseInt(arr[0])+Integer.parseInt(arr[1]);
+      System.out.println(String.format("Case #%d: %s + %s = %d",i+1,arr[0],arr[1],result ));
 
     }
 
