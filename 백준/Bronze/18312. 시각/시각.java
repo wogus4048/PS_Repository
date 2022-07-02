27cf00d89991a5a6fd83506f527e11ddbc809390
wogus4048 <67767276+wogus4048@users.noreply.github.com>
2022-07-02 18:15:57 +0900

    [Bronze II] Title: 시각, Time: 312 ms, Memory: 29532 KB -BaekjoonHub

--- 백준/Bronze/18312. 시각/시각.java
+++ 백준/Bronze/18312. 시각/시각.java
@@ -10,34 +10,73 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 
 public class Main {
+  static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+  static int n;
+  static String k;
+  static int result = 0;
+
   public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-    int n = Integer.parseInt(st.nextToken());
-    String k = st.nextToken();
-    int result = 0;
-
-    for (int i = 0; i <= n; i++) {
-      String check2 = String.format("%02d", i);
-      if (check2.contains(k)) {
-        result += 3600;
-      } else {
-        for (int j = 0; j < 60; j++) {
-          for (int a = 0; a < 60; a++) {
-            String check = String.format("%02d", i) + String.format("%02d", j) + String.format("%02d", a);
-            if (check.contains(k)) {
-              result += 1;
-            }
-          }
-        }
-      }
-    }
+    input();
+    solve();
 
     bw.write(result + "\n");
     bw.flush();
     bw.close();
+
+  }
+
+  static void input() throws IOException {
+    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    n = Integer.parseInt(st.nextToken());
+    k = st.nextToken();
+
+  }
+
+  static void solve() throws IOException {
+    int h = 0;
+    int m = 0;
+    int s = 0;
+
+    String time = "000000";
+
+    while (h <= n) {
+
+      if (time.contains(k)) {
+        result += 1;
+      }
+
+      ///
+      if (++s == 60) {
+        s = 0;
+        if (++m == 60) {
+          m = 0;
+          h++;
+        }
+      }
+      ///
+      time = "";
+
+      if (h < 10) {
+        time += "0" + h;
+      } else {
+        time += h;
+      }
+
+      if (m < 10) {
+        time += "0" + m;
+      } else {
+        time += m;
+      }
+      if (s < 10) {
+        time += "0" + s;
+      } else {
+        time += s;
+      }
+      // bw.write(time + "\n");
+    }
+
   }
 
 }
