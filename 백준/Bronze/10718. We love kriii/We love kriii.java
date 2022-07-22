fd506482e26db17d24503e52ee3bd568a8be6568
wogus4048 <67767276+wogus4048@users.noreply.github.com>
2022-07-22 16:57:59 +0900

    [Bronze V] Title: We love kriii, Time: 124 ms, Memory: 14020 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,39 @@
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.Stack;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+
+public class Main {
+  static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+  public static void main(String[] args) throws IOException {
+
+    input();
+    solve();
+    // test();
+
+    bw.flush();
+    br.close();
+    bw.close();
+  }
+
+  static void input() throws IOException {
+
+  }
+
+  static void solve() throws IOException {
+    bw.write("강한친구 대한육군\n");
+    bw.write("강한친구 대한육군\n");
+
+  }
+
+  static void test() throws IOException {
+
+  }
+
+}
