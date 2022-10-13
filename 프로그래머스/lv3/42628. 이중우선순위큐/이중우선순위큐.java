e6e1d6d6d82b7d941cf19aed1736ebfa4ba30b37
wogus4048 <67767276+wogus4048@users.noreply.github.com>
2022-10-13 14:49:59 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.33 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,57 +1,55 @@
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
 
-class Solution {
+
+import java.util.*;
+
+public class Solution {
     public static int[] solution(String[] operations) {
-
-        PriorityQueue<Integer> minpq = new PriorityQueue<>(); //최소힙
-        PriorityQueue<Integer> maxpq = new PriorityQueue<>(Collections.reverseOrder()); //최대힙
-
+        int[] answer = {};
+        List<Integer> a = new ArrayList<>();
 
         for(int i=0;i<operations.length;i++)
         {
-            StringTokenizer st = new StringTokenizer(operations[i]); //띄어쓰기로 구분
-            String next = st.nextToken();
-            if(next.equals("I")) //숫자 삽입
-            {
-                int next_number = Integer.parseInt(st.nextToken());
-                //둘다 넣어준다.
-                minpq.add(next_number);
-                maxpq.add(next_number);
+            StringTokenizer st = new StringTokenizer(operations[i]);
 
-            }else //삭제 일경우
+            String operation = st.nextToken();
+            int number = Integer.parseInt(st.nextToken());
+
+            if(operation.equals("I"))
             {
-                if(minpq.size()<1) //비어있는데 삭제하라고 하면 넘어감
+                a.add(number);
+            }
+            else{
+                if(a.isEmpty())
                 {
                     continue;
                 }
+                else{
+                    if(number == 1)
+                    {
+                        a.remove(a.size()-1);
+                    }
+                    else{
+                        a.remove(0);
 
-                int next_number = Integer.parseInt(st.nextToken());
-
-                if(next_number == 1) //최대값 삭제라면
-                {
-                    int max = maxpq.poll(); //최대힙에서 하나 삭제해주고
-                    minpq.remove(max); // 최소힙에서도 해당 값삭제
-
-                }else {
-                    int min = minpq.poll(); //최대힙에서 하나 삭제해주고
-                    maxpq.remove(min); // 최소힙에서도 해당 값삭제
-
+                    }
                 }
 
-            }
-        }
 
-        if(maxpq.isEmpty()) //큐가 비어있다면 --> [0,0] 리턴
+            }
+            Collections.sort(a);
+
+
+        }
+        if(a.isEmpty())
         {
-            return new int[]{0,0};
+            return new int[] {0,0};
         }
         else{
-            return new int[] {maxpq.peek(),minpq.peek()};
+            return new int[] {a.get(a.size()-1) , a.get(0)};
         }
+
+
+
 
 
 
@@ -60,9 +58,7 @@ class Solution {
 
     public static void main(String[] args) {
         String[] operation = {"I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"};
-        System.out.println(Arrays.toString(solution(operation)) );
-
-
+        System.out.println(Arrays.toString(solution(operation)) ) ;
     }
 
 
