6d37fcf99eee8ba82265cd8281f63cce126a087f
wogus4048 <67767276+wogus4048@users.noreply.github.com>
2022-10-20 12:02:27 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 282.56 ms, Memory: 96.1 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,10 +1,16 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class Solution {
+
     public static String solution(String[] participant, String[] completion) {
         String answer = "";
-        Arrays.sort(participant);
-        Arrays.sort(completion);
+        List<String> par = Arrays.asList(participant);
+        List<String> com = Arrays.asList(completion);
+        
+        Collections.sort(par);  // Collections.sort는 최악 nlogn     // Arrays.sort는 최악 n^2  //둘다 통과는됨
+        Collections.sort(com);
 
         for(int i=0;i<participant.length-1;i++)
         {
@@ -14,6 +20,7 @@ public class Solution {
             }
         }
         answer = participant[participant.length-1];
+        
 
 
         return answer;
