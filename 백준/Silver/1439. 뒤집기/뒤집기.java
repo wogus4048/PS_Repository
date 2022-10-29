b2ba6abb32ec713ec548e072a3a57c31920fa37e
wogus4048 <67767276+wogus4048@users.noreply.github.com>
2022-10-29 14:22:36 +0900

    [Silver V] Title: 뒤집기, Time: 132 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.java
+++ 백준/Silver/1439. 뒤집기/뒤집기.java
@@ -1,5 +1,4 @@
 
-
 import java.io.*;
 import java.lang.reflect.Array;
 import java.util.*;
@@ -14,42 +13,18 @@ public class Main {
         int result1 = 0;
         int result0 = 0;
 
-        Stack<Integer> list = new Stack<>();
-        for (int i = 0; i < input.length(); i++) {
-            int temp = Character.getNumericValue(input.charAt(i));
-            list.push(temp);
-        }
-//        bw.write(list+"\n");
+        StringTokenizer st = new StringTokenizer(input,"0");
+        result0 = st.countTokens();
+        st = new StringTokenizer(input, "1");
+        result1 = st.countTokens();
 
-        int ex = -1;
-
-        while(!list.isEmpty())
-        {
-            int current = list.pop();
-            if(ex != -1)
-            {
-                if (current == ex) {
-                    continue;
-                }
-            }
-            if(current == 0)
-            {
-                result0++;
-                ex = current;
-            }
-            else {
-                result1++;
-                ex = current;
-            }
-        }
-
-        if(result0>result1)
-        {
-            bw.write(result1+"");
-        }
-        else{
+        if (result1 > result0) {
             bw.write(result0+"");
         }
+        else{
+            bw.write(result1+"");
+        }
+
 
 
 
