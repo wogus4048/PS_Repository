ce43719d07e67e31590a17e084b5bc58766e02e3
wogus4048 <67767276+wogus4048@users.noreply.github.com>
2022-11-03 13:46:20 +0900

    [level 2] Title: 타겟 넘버, Time: 1.59 ms, Memory: 71.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,24 @@
+public class Solution {
+    int answer = 0;
+    public int solution(int[] numbers, int target) {
+
+
+
+        dfs(numbers,0,target,0);
+
+        return answer;
+    }
+
+    public void dfs(int[] numbers, int depth, int target, int sum){
+        if(depth == numbers.length){ // 마지막 노드까지 탐색한 경우
+            if(target == sum) answer++;
+        } else { // 탐색할 노드가 남아있는 경우
+            dfs(numbers, depth + 1, target, sum + numbers[depth]); // 해당 노드의 값을 더하고 다음 깊이 탐색
+            dfs(numbers, depth + 1, target, sum - numbers[depth]); // 해당 노드의 값을 빼고 다음 깊이 탐색
+        }
+    }
+
+
+
+
+}
