ef9d5cf8fd01f374b3fce6a5e9c7159a99be78ce
Gil Jae Hyun <67767276+wogus4048@users.noreply.github.com>
2022-12-04 13:12:29 +0900

    [Silver V] Title: 소트인사이드, Time: 124 ms, Memory: 14372 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -2,7 +2,6 @@ import java.io.*;
 import java.lang.reflect.Array;
 import java.util.*;
 import java.util.Map.Entry;
-
 public class Main {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -10,18 +9,17 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         String input = br.readLine();
-        Integer[] numberArray = new Integer[input.length()];
+        Character[] charArray = new Character[input.length()];
 
         for (int i = 0; i < input.length(); i++) {
-            numberArray[i] = Character.getNumericValue(input.charAt(i));
+            charArray[i] = input.charAt(i);
+        }
+        Arrays.sort(charArray, Comparator.reverseOrder());
+
+        for (char current : charArray) {
+            bw.write(current+"");
         }
 
-        Arrays.sort(numberArray, Comparator.reverseOrder());
-
-        for (int number : numberArray) {
-            bw.write(number+"");
-        }
-        
 
         bw.flush();
         bw.close();
