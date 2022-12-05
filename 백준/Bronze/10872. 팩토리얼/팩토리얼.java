2795abb56db6bb3bd41a04480a798483f7abd12a
Gil Jae Hyun <67767276+wogus4048@users.noreply.github.com>
2022-12-05 18:56:06 +0900

    [Bronze V] Title: 팩토리얼, Time: 128 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.java
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -1,36 +1,24 @@
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.Map.Entry;
 
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-public class Main{
-  public static void main(String[] args) throws IOException{
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        int sum =1;
+        for (int i = 1; i <= n; i++) {
+            sum *= i;
+        }
+        bw.write(sum+"");
+        
+        bw.flush();
+        bw.close();
 
-    int number = Integer.parseInt(br.readLine());
-
-    bw.write(fac(number)+"\n");
-
-      
-      
-    bw.flush();
-    bw.close();
-    
-  }
-
-  static int fac(int num)
-  {
-    if(num == 0 | num ==1)
-    { 
-      return 1;
     }
-    return num * fac(num-1);
-  }
-    
-  }
 
 
+}
