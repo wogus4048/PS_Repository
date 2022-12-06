f6ab925f4cce46dd19d543c9bc6fd932e360d12a
Gil Jae Hyun <67767276+wogus4048@users.noreply.github.com>
2022-12-06 20:53:01 +0900

    [Silver V] Title: 덩치, Time: 144 ms, Memory: 15916 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.java
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -1,67 +1,46 @@
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.StringTokenizer;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.Map.Entry;
 
 public class Main {
-  static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-  static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-  static int test_case;
-  static int[] rank;
-  static String[][] arr;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-  public static void main(String[] args) throws IOException {
 
-    input();
-    solve();
-    // test();
-    for (int number : rank) {
-      bw.write(number + " ");
-    }
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
 
-    bw.flush();
-    br.close();
-    bw.close();
-  }
+        int[][] data = new int[n][2];
 
-  static void input() throws IOException {
-    test_case = Integer.parseInt(br.readLine());
-    arr = new String[test_case][];
-    for (int i = 0; i < test_case; i++) {
-      arr[i] = br.readLine().split(" ");
-    }
-    rank = new int[test_case];
-    Arrays.fill(rank, 1);
-  }
-
-  static void solve() throws IOException {
-    for (int i = 0; i < test_case; i++) {
-
-      for (int j = i + 1; j < test_case; j++) {
-        if (i != test_case - 1) {
-
-          if (Integer.parseInt(arr[i][0]) > Integer.parseInt(arr[j][0])) {
-            if (Integer.parseInt(arr[i][1]) > Integer.parseInt(arr[j][1])) {
-              rank[j] += 1;
-            }
-          }
-          if (Integer.parseInt(arr[i][0]) < Integer.parseInt(arr[j][0])) {
-            if (Integer.parseInt(arr[i][1]) < Integer.parseInt(arr[j][1])) {
-              rank[i] += 1;
-            }
-          }
+        for (int i = 0; i < n; i++) {
+            String[] input = br.readLine().split(" ");
+            data[i][0] = Integer.parseInt(input[0]);
+            data[i][1] = Integer.parseInt(input[1]);
         }
 
-      }
+        int[] rating = new int[n];
+        Arrays.fill(rating, 1);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i != j) {
+                    if (data[i][0] < data[j][0] && data[i][1] < data[j][1]) {
+                        rating[i] ++;
+                    }
+                }
+            }
+        }
+
+        for (int rank : rating) {
+            bw.write(rank+" ");
+        }
+
+
+        bw.flush();
+        bw.close();
 
     }
 
-  }
 
-  static void test() throws IOException {
-
-  }
 }
