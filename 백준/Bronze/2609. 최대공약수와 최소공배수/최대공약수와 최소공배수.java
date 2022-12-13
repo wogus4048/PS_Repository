bb852c9b1ea536be0ae1115c869df52892233e13
Gil Jae Hyun <67767276+wogus4048@users.noreply.github.com>
2022-12-13 10:02:42 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 152 ms, Memory: 16140 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.Map.Entry;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+    public static void main(String[] args) throws IOException {
+        String[] input = br.readLine().split(" ");
+        int num1 = Integer.parseInt(input[0]);
+        int num2 = Integer.parseInt(input[1]);
+
+        bw.write(gcd(num1, num2) + "\n" + lcm(num1, num2));
+
+        bw.flush();
+        bw.close();
+
+    }
+
+    //최대공약수
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+
+    public static int lcm(int a, int b) {
+        return a * b / gcd(a, b);
+    }
+
+}
