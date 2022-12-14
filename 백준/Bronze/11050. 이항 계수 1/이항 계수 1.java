29fea275dc2fb6011abc72428e8c8723412247f2
Gil Jae Hyun <67767276+wogus4048@users.noreply.github.com>
2022-12-14 15:34:56 +0900

    [Bronze I] Title: 이항 계수 1, Time: 128 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -1,64 +1,46 @@
 import java.io.*;
-import java.lang.reflect.Array;
-import java.util.*;
-import java.util.Map.Entry;
 
 public class Main {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    static int result;
 
+   static int n;
+   static int k;
+   static int result;
+    static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
         String[] input = br.readLine().split(" ");
-        int n = Integer.parseInt(input[0]);
-        int k = Integer.parseInt(input[1]);
+        n = Integer.parseInt(input[0]);
+        k = Integer.parseInt(input[1]);
 
-        boolean[] visited = new boolean[n];
-        if (k == 0) {
-            bw.write(1+"");
-        }
-        else{
-            combi2(k,0,0,n);
-            bw.write(result+"");
+        visited = new boolean[n];
 
-        }
-
-
-
+        combination(0, 0);
+        bw.write(result+"");
 
         bw.flush();
         bw.close();
 
     }
 
-    //조합
-    public static void combi(int target, int count,int start, boolean[] visited) {
-        if (count == target) {
+    public static void combination( int count,int depth) {
+        if (count == k) { //다 선택한경우
             result++;
             return;
         }
-        for (int i = start; i < visited.length; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                combi(target, count + 1,i+1, visited);
-                visited[i] = false;
-            }
-        }
-
-    }
-    public static void combi2(int target, int count,int start,int n) {
-        if (count == target) {
-            result++;
+        if (depth == n) {
             return;
         }
-        for (int i = start; i < n; i++) {
-            combi2(target, count + 1,i+1,n);
-        }
+        visited[depth] = true;
+        combination(count + 1, depth + 1);
+        visited[depth] = false;
+        combination(count, depth + 1);
 
     }
+
 
 
 }
