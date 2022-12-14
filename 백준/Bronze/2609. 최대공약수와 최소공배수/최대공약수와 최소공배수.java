c6d87c3bbde658b36187a3da4ad0393f0ab4cc4c
Gil Jae Hyun <67767276+wogus4048@users.noreply.github.com>
2022-12-14 14:15:03 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 152 ms, Memory: 16120 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,7 +1,4 @@
 import java.io.*;
-import java.lang.reflect.Array;
-import java.util.*;
-import java.util.Map.Entry;
 
 public class Main {
 
@@ -11,17 +8,17 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         String[] input = br.readLine().split(" ");
-        int num1 = Integer.parseInt(input[0]);
-        int num2 = Integer.parseInt(input[1]);
+        int a = Integer.parseInt(input[0]) ;
+        int b = Integer.parseInt(input[1]);
 
-        bw.write(gcd(num1, num2) + "\n" + lcm(num1, num2));
+        int l = (a * b) / gcd(a, b);
+        bw.write(gcd(a, b) + "\n" + l );
 
         bw.flush();
         bw.close();
 
     }
 
-    //최대공약수
     public static int gcd(int a, int b) {
         if (b == 0) {
             return a;
@@ -29,8 +26,5 @@ public class Main {
         return gcd(b, a % b);
     }
 
-    public static int lcm(int a, int b) {
-        return a * b / gcd(a, b);
-    }
 
 }
