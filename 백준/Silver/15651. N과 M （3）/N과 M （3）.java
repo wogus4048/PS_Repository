71cafaad3686b7118395c358754801fb5ba8fa45
Gil Jae Hyun <67767276+wogus4048@users.noreply.github.com>
2022-12-15 14:03:05 +0900

    [Silver III] Title: N과 M (3), Time: 572 ms, Memory: 158688 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,44 +1,38 @@
 import java.io.*;
-import java.lang.reflect.Array;
-import java.math.BigInteger;
-import java.util.Arrays;
 
 public class Main {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+    /**
+     * 중복 순열
+     * 중복을 막는 방문배열을 없앤다.
+     */
 
     public static void main(String[] args) throws IOException {
         String[] input = br.readLine().split(" ");
         int n = Integer.parseInt(input[0]);
-        int m = Integer.parseInt(input[1]);
-
-        boolean[] visited = new boolean[n];
-        int[] result = new int[m];
-        combi(n,m,0,visited,result,0);
-
-
-
+        int r = Integer.parseInt(input[1]);
+        int[] result = new int[r];
+        permutation(n, r,  result, 0);
         bw.flush();
         bw.close();
 
     }
 
-    public static void combi(int n, int r,int start,boolean[] visited,int[] result,int depth) throws IOException{
-        if (r == depth) {
+    public static void permutation(int n, int r,int[] result,int count) throws  IOException{
+        if (count == r) {
             for (int i = 0; i < r; i++) {
-                bw.write(result[i]+" ");
+                bw.write(result[i] + " ");
             }
             bw.newLine();
             return;
         }
         for (int i = 0; i < n; i++) {
-            visited[i] = true;
-            result[depth] = i+1;
-            combi(n, r,i+1 ,visited,result,depth+1);
-            visited[i] = false;
+            result[count] = i + 1; //결과값을 저장할때 인덱스는 지금까지 뽑은갯수여야만 한다.
+            permutation(n, r,result,count+1);
         }
+
     }
-    
+
 }
