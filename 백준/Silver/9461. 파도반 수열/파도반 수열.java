71edc42f0c516a50ee660d8baec42f03d68a16d4
Gil Jae Hyun <67767276+wogus4048@users.noreply.github.com>
2022-12-16 17:37:41 +0900

    [Silver III] Title: 파도반 수열, Time: 144 ms, Memory: 14628 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -5,42 +5,51 @@ public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    //P1 + p5 = p6
-    //p2 + p6 = p7
-    //p3 + p6 = p8
-    // ... 반복
 
+    /*
+    어떤 규칙이 있나 살펴보면 f(n) = f(n-1) + f(n-5)의 규칙을 알 수 있다.
+    예를들어 7이 어떻게 구해졌나 보면 바로 뒤숫자인 5와 , 5번째전인 2와 합쳐져서 생성된다.
+    규칙을 찾았으니 규칙을 가지고 메모이제이션을 진행해서 풀이해보자.
+     */
 
-    static long[] pado = new long[101];
+    static int testcase;
+    static int n;
+    static long[] memoization = new long[101];
 
     public static void main(String[] args) throws IOException {
-        int n = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < n; i++) {
-            bw.write(getPado(Integer.parseInt(br.readLine()))+"\n");
-        }
+        input();
+        solve();
 
         bw.flush();
         bw.close();
 
     }
 
-    public static long getPado(int n) {
-        pado[1] = 1;
-        pado[2] = 1;
-        pado[3] = 1;
-        pado[4] = 2;
-        pado[5] = 2;
-
-        int start = 1;
-        for (int i = 6; i <= n; i++) {
-            pado[i] = pado[start] + pado[i - 1];
-            start++;
-        }
-        return pado[n];
-
+    static void input() throws IOException {
+        testcase = Integer.parseInt(br.readLine());
+        //기저 상태 넣어놓기
+        memoization[1] = 1;
+        memoization[2] = 1;
+        memoization[3] = 1;
+        memoization[4] = 2;
+        memoization[5] = 2;
+        memoization[6] = 3;
     }
 
+    static void solve() throws IOException {
+        for (int i = 0; i < testcase; i++) {
+            n = Integer.parseInt(br.readLine());
+            bottomUp(n);
+            bw.write(memoization[n]+"\n");
+        }
+    }
 
-
+    static void bottomUp(int n) {
+        for (int i = 7; i <=n; i++) {
+            if (memoization[i] == 0) { // 다른 테스트케이스 덕분에 이미 채워져있을수도 있으니까.
+                memoization[i] = memoization[i - 1] + memoization[i - 5];
+            }
+        }
+    }
 }
