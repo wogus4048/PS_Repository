032e1f0ae7953520c17d9c0b395867b58205af20
Gil Jae Hyun <67767276+wogus4048@users.noreply.github.com>
2022-12-20 14:01:03 +0900

    [Silver IV] Title: 스택, Time: 236 ms, Memory: 20868 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,69 +1,70 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.util.Stack;
-import java.util.StringTokenizer;
 
 public class Main {
-  static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-  static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-  static int num;
-  static Stack<Integer> stack = new Stack<>();
 
-  public static void main(String[] args) throws IOException {
-    input();
-    solve();
-    // test();
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    bw.flush();
-    bw.close();
 
-  }
+    /*
 
-  public static void input() throws IOException {
-    num = Integer.parseInt(br.readLine());
+     */
 
-  }
+    static int n;
+    static Stack<Integer> stack;
 
-  public static void solve() throws IOException {
+    public static void main(String[] args) throws IOException {
 
-    for (int i = 0; i < num; i++) {
-      StringTokenizer st = new StringTokenizer(br.readLine());
-      String input = st.nextToken();
-      if (input.equals("push")) {
-        stack.push(Integer.parseInt(st.nextToken()));
-      } else if (input.equals("top")) {
-        if (stack.isEmpty()) {
-          bw.write(-1 + "\n");
-        } else {
-          bw.write(stack.peek() + "\n");
-        }
-      }
+        input();
+        solve();
 
-      else if (input.equals("size")) {
-        bw.write(stack.size() + "\n");
-      } else if (input.equals("empty")) {
-        if (stack.isEmpty()) {
-          bw.write("1\n");
-        } else {
-          bw.write("0\n");
-        }
-      } else if (input.equals("pop")) {
-        if (stack.isEmpty()) {
-          bw.write("-1\n");
-        } else {
-          bw.write(stack.pop() + "\n");
-        }
-      }
+        bw.flush();
+        bw.close();
 
     }
 
-  }
+    static void input() throws IOException {
+        n = Integer.parseInt(br.readLine());
+        stack = new Stack<>();
+    }
 
-  static void test() throws IOException {
+    static void solve() throws IOException {
+        for (int i = 0; i < n; i++) {
+            String[] input = br.readLine().split(" ");
+            if (input[0].equals("push")) {
+                stack.push(Integer.parseInt(input[1]));
+            }
+            if (input[0].equals("top")) {
+                if (stack.isEmpty()) {
+                    bw.write(-1 + "\n");
+                } else {
+                    bw.write(stack.peek()+"\n");
+                }
 
-  }
+            }
+            if (input[0].equals("size")) {
+                bw.write(stack.size()+"\n");
+
+            }if (input[0].equals("empty")) {
+                if (stack.isEmpty()) {
+                    bw.write(1 + "\n");
+                } else {
+                    bw.write(0+"\n");
+                }
+
+            }if (input[0].equals("pop")) {
+                if (stack.isEmpty()) {
+                    bw.write(-1 + "\n");
+                } else {
+                    bw.write(stack.pop()+"\n");
+                }
+
+            }
+        }
+
+    }
+
+
 
 }
