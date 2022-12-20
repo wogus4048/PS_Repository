54016f28af9a082c42a63efdea177d4d45138e44
Gil Jae Hyun <67767276+wogus4048@users.noreply.github.com>
2022-12-20 14:56:03 +0900

    [Silver IV] Title: 카드2, Time: 196 ms, Memory: 45640 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,52 +1,50 @@
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
+
 
 public class Main {
-  static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-  static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-  static int n;
-  static Queue<Integer> qq = new LinkedList<>();
 
-  public static void main(String[] args) throws IOException {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    input();
-    solve();
-    // test();
 
-    bw.flush();
-    br.close();
-    bw.close();
-  }
+    /*
 
-  static void input() throws IOException {
-    n = Integer.parseInt(br.readLine());
+     */
+    static int n;
+    static Queue<Integer> queue = new LinkedList<>();
 
-  }
+    public static void main(String[] args) throws IOException {
 
-  static void solve() throws IOException {
-    for (int i = 1; i <= n; i++) {
-      qq.add(i);
-    }
-    while (qq.size() > 2) {
-      qq.remove();
-      qq.add(qq.poll());
+        input();
+        solve();
+
+        bw.flush();
+        bw.close();
 
     }
-    if (qq.size() == 2) {
-      qq.remove();
-      bw.write(qq.peek() + "\n");
-    } else if (qq.size() == 1) {
-      bw.write(qq.peek() + "\n");
+
+    static void input() throws IOException {
+        n = Integer.parseInt(br.readLine());
+        if (n == 1) {
+            bw.write(1+"");
+        }
+        for (int i = 1; i <= n; i++) {
+            queue.add(i);
+        }
     }
 
-  }
+    static void solve() throws IOException {
+        while (queue.size()>=2) {
+            queue.poll();
+            if (queue.size() == 1) {
+                bw.write(queue.poll() + "");
+                break;
+            } else {
+                queue.add(queue.poll());
+            }
+        }
+    }
 
-  static void test() throws IOException {
-
-  }
 }
