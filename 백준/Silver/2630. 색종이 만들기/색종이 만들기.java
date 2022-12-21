0b313dfac3517fdae96c36b5ab4db65c6efba73d
Gil Jae Hyun <67767276+wogus4048@users.noreply.github.com>
2022-12-21 13:25:01 +0900

    [Silver II] Title: 색종이 만들기, Time: 216 ms, Memory: 18656 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -1,70 +1,69 @@
-import java.util.Scanner;
- 
- 
+import java.io.*;
+import java.util.Arrays;
+
+
 public class Main {
-	
-	// 색상 카운트 할 변수 및 색종이(board)
-	public static int white = 0;
-	public static int blue = 0;
-	public static int[][] board;
- 
-	public static void main(String[] args) {
-		
-		Scanner in = new Scanner(System.in);
-		
-		int N = in.nextInt();
-		
-		board = new int[N][N];
-		
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < N; j++) {
-				board[i][j] = in.nextInt();
-			}
-		}
-		
-		partition(0, 0, N);
-		
-		System.out.println(white);
-		System.out.println(blue);
-		
-	}
-	
-	public static void partition(int row, int col, int size) {
-		
-		//
-		if(colorCheck(row, col, size)) {
-			if(board[row][col] == 0) {
-				white++;
-			}
-			else {
-				blue++;
-			}
-			return;
-		}
-		
-		int newSize = size / 2;	// 절반 사이즈
-		// 재귀 호출
-		partition(row, col, newSize);						// 2사분면
-		partition(row, col + newSize, newSize);				// 1사분면
-		partition(row + newSize, col, newSize);				// 3사분면
-		partition(row + newSize, col + newSize, newSize);	// 4사분면
-	}
-	
-	
-	// 현재 파티션의 컬러가 같은지 체크한다.
-	public static boolean colorCheck(int row, int col, int size) {
-	
-		int color = board[row][col];	// 첫 번째 원소를 기준으로 검사
-		
-		for(int i = row; i < row + size; i++) {
-			for(int j = col; j < col + size; j++) {
-				// 색상이 같이 않다면 false를 리턴 
-				if(board[i][j] != color) {
-					return false;
-				}
-			}
-		}
-		// 검사가 모두 통과했다는 의미이므로 true 리턴
-		return true;
-	}
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+    /*
+    현재 네모칸이 모두 1(파란색) 인지 0(흰색)인지 체크하고 아니면 가로세로를 반반나눠서 다시 체크한다.
+    모두 1 또는 0이라면 어떤 색종이인지 체크.
+     */
+    static int[][] map;
+    static int blue;
+    static int white;
+    static int size;
+
+
+    public static void main(String[] args) throws IOException {
+
+        input();
+        solve();
+
+        bw.flush();
+        bw.close();
+
+    }
+
+    static void input() throws IOException {
+        size = Integer.parseInt(br.readLine());
+        map = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            for (int j = 0; j < size; j++) {
+                map[i][j] = input[j];
+            }
+        }
+    }
+
+    static void solve() throws IOException {
+
+        checkColor(0, 0,size);
+        bw.write(white+"\n"+blue);
+    }
+    static void checkColor(int x, int y,int n) throws IOException {
+        int color = map[x][y];
+        for (int i = x; i < x+n; i++) {
+            for (int j = y; j < y+n; j++) {
+                if (map[i][j] != color) {
+                    checkColor(x, y, n/2); //왼쪽 위
+                    checkColor(x + (n / 2), y ,n/2); //오른쪽위
+                    checkColor(x , y + (n / 2),n/2); //왼쪽아래
+                    checkColor(x + (n / 2), y + (n / 2),n/2); //오른쪽아래
+                    return;
+                }
+            }
+        }
+
+        if (color == 1) {
+            blue++;
+        } else {
+            white++;
+        }
+    }
+
+
 }
