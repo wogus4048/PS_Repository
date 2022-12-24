3304cddc0959f7cb1393ccf483671093d27ae9a4
Gil Jae Hyun <67767276+wogus4048@users.noreply.github.com>
2022-12-24 13:29:02 +0900

    [Silver II] Title: 최소 힙, Time: 364 ms, Memory: 26504 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+    /*
+
+     */
+    static PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+    static int n;
+
+    public static void main(String[] args) throws IOException {
+
+        input();
+        solve();
+
+        bw.flush();
+        bw.close();
+
+    }
+
+    static void input() throws IOException {
+        n = Integer.parseInt(br.readLine());
+    }
+
+    static void solve() throws IOException {
+
+        for (int i = 0; i < n; i++) {
+            int input = Integer.parseInt(br.readLine());
+            if (input == 0) {
+                if (!priorityQueue.isEmpty()) {
+                    bw.write(priorityQueue.poll() + "\n");
+                } else {
+                    bw.write("0\n");
+                }
+            } else {
+                priorityQueue.add(input);
+            }
+        }
+
+
+    }
+
+}
