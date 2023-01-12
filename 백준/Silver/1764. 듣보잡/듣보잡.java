4a573bd981d9137bde95ad754781c8c4ad78c16a
Gil Jae Hyun <67767276+wogus4048@users.noreply.github.com>
2023-01-12 19:42:18 +0900

    [Silver IV] Title: 듣보잡, Time: 336 ms, Memory: 26692 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -1,63 +1,53 @@
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
-import java.util.StringTokenizer;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
 
 public class Main {
-  static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-  static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-  static int N;
-  static int M;
-  static HashSet<String> N_people = new HashSet<>();
-  static ArrayList<String> Result = new ArrayList<>();
 
-  public static void main(String[] args) throws IOException {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static Set<String> names = new HashSet<>();
+    static List<String> answer = new ArrayList<>();
+    static int n,m;
+    /*
+     */
 
-    input();
-    solve();
-    // test();
+    public static void main(String[] args) throws IOException {
+        input();
+        solve();
 
-    bw.flush();
-    br.close();
-    bw.close();
-  }
-
-  static void input() throws IOException {
-    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-    N = Integer.parseInt(st.nextToken());
-    M = Integer.parseInt(st.nextToken());
-    for (int i = 0; i < N; i++) {
-      N_people.add(br.readLine());
-    }
-
-  }
-
-  static void solve() throws IOException {
-    String temp = "";
-    for (int i = 0; i < M; i++) {
-      temp = br.readLine();
-      if (N_people.contains(temp)) {
-        Result.add(temp);
-      }
+        bw.flush();
+        bw.close();
 
     }
-    Collections.sort(Result);
-    bw.write(Result.size() + "\n");
-    for (int j = 0; j < Result.size(); j++) {
-      bw.write(Result.get(j) + "\n");
+
+    static void input() throws IOException {
+        int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        n = input[0];
+        m = input[1];
+        for (int i = 0; i < n; i++) {
+            String name = br.readLine();
+            names.add(name);
+        }
+
     }
+    static void solve() throws IOException {
+        for (int i = 0; i < m; i++) {
+            String name = br.readLine();
+            if (names.contains(name)) {
+                answer.add(name);
+            }
+        }
+        bw.write(answer.size() + "\n");
+        answer.sort(Comparator.naturalOrder());
+        for (String name : answer) {
+            bw.write(name+"\n");
+        }
 
-  }
-
-  static void test() throws IOException {
-
-  }
-
+    }
 }
