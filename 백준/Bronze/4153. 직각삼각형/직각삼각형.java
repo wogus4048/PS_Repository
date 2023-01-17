0c7ed4543b8ffe1aab01bdefce218e0bf26f6b3c
Gil Jae Hyun <67767276+wogus4048@users.noreply.github.com>
2023-01-17 10:53:25 +0900

    [Bronze III] Title: 직각삼각형, Time: 132 ms, Memory: 14444 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.java
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -1,39 +1,49 @@
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.util.Arrays;
-import java.util.StringTokenizer;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 
-public class Main{
-  public static void main(String[] args) throws IOException{
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+public class Main {
 
-    while(true)
-    {
-      StringTokenizer st = new StringTokenizer(br.readLine()," ");
-      int[] arr = {Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken())};
-      Arrays.sort(arr);
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static Map<Integer, Integer> xMap = new HashMap<>();
+    static Map<Integer, Integer> yMap = new HashMap<>();
 
-      if(arr[0] == 0 && arr[1] == 0 && arr[2] == 0)
-      {
-        break;
-      }
+    /*
+     */
 
-      String result = (  (Math.pow(arr[0],2) + Math.pow(arr[1],2)) == Math.pow(arr[2],2) ) ? "right":"wrong";
-  
-      bw.write(result+"\n");
+    public static void main(String[] args) throws IOException {
+        input();
+        solve();
+
+        bw.flush();
+        bw.close();
 
     }
-    
-    bw.flush();
-    bw.close();
 
-    
-  }
+    static void input() throws IOException {
+    }
+    static void solve() throws IOException {
+        while (true) {
+            int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt)
+                .toArray();
+            if (input[0] == 0 && input[1] == 0 && input[2] == 0) {
+                break;
+            }
+            Arrays.sort(input);
+
+            if (Math.pow((double) input[0], 2.0) + Math.pow((double) input[1], 2.0) == Math.pow(
+                (double) input[2], 2.0)) {
+                System.out.println("right");
+            } else {
+                System.out.println("wrong");
+            }
+        }
 
 
+    }
 }
