6864ca313ae972bec2c27e6c35708fe6cd16363d
Gil Jae Hyun <67767276+wogus4048@users.noreply.github.com>
2023-03-28 18:25:53 +0900

    [level 2] Title: 위장, Time: 0.31 ms, Memory: 86 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42578. 위장/위장.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.util.Map.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        Map<String,Integer> map = new HashMap<>();
+        
+        for(String[] cloth : clothes)
+        {
+            String category = cloth[1];
+            map.put(category,map.getOrDefault(category,0)+1);
+        }
+        System.out.println(map);
+        
+        List<Integer> valueList = new ArrayList<>(map.values());
+        //values()는 Collection객체로 리턴되므로 List에 담아서 list로 사용한다.
+
+        
+        for(int count : valueList)
+        {
+            answer *= (count+1);
+        }
+        
+        
+        return answer-1;
+    }
+}
+
+/*
+Map<카테고리,갯수>로 정리 한 후 
+
+각 카테고리 갯수+1 를 서로 곱해준다. 그리고 하나도 입지않는경우를 빼준다 (-1)
+
+*/
