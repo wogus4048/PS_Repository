021925b65b35f2721f80ae0b62f58476e5502217
Gil Jae Hyun <67767276+wogus4048@users.noreply.github.com>
2023-03-28 17:51:23 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 75.70 ms, Memory: 95.8 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,7 +1,47 @@
 import java.util.*;
+import java.util.Map.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        
+        Map<String,Integer> part = new HashMap<>();
+        Map<String,Integer> comp = new HashMap<>();
+        
+        for(String name : participant)
+        {
+            part.put(name,part.getOrDefault(name,0)+1);
+        }
+        for(String name : completion)
+        {
+            comp.put(name,comp.getOrDefault(name,0)+1);
+        }
+        // System.out.println(part);
+        // System.out.println(comp);
+        
+        for(Entry<String,Integer> entry : part.entrySet())
+            //entrySet을 사용하려면 import java.util.Map.*; 필수 
+        {
+            String key = entry.getKey();
+            int value = entry.getValue();
+            if(!comp.containsKey(key))
+            {
+                return key;
+            }
+            if(comp.get(key) != value)
+            {
+                return key;
+            }
+        }
+        
+       return "";
+    }
+}
+/*
+1.
+참가자,완주자 명단을 정렬
+참가자명단을 반복문을 돌면서 같은 인덱스에 완주자 명단을 보고 이름이 같은지 체크 아니면 완주 못한사람 
+public String solution(String[] participant, String[] completion) {
         String answer = "";
         
         if(participant.length == 1)
@@ -25,12 +65,6 @@ class Solution {
         
         return participant[participant.length-1];
     }
-}
-/*
-1.
-참가자,완주자 명단을 정렬
-참가자명단을 반복문을 돌면서 같은 인덱스에 완주자 명단을 보고 이름이 같은지 체크 아니면 완주 못한사람 
-
 2.
 Map 2개만들어서 각각 참가자 ,완주자 정리
 참가자 Map을 돌면서 해당 키가 존재하는지, 존재하면 value의 값이 같은지 체크 아니면 완주 못한사람 
