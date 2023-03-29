26506d4fb14b1f26d0eb9a9e101e048b940a84d1
Gil Jae Hyun <67767276+wogus4048@users.noreply.github.com>
2023-03-29 16:51:24 +0900

    [level 2] Title: 올바른 괄호, Time: 20.40 ms, Memory: 53.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,34 +1,34 @@
 import java.util.*;
-public class Solution {
+
+class Solution {
     boolean solution(String s) {
         boolean answer = true;
-
         Stack<Character> stack = new Stack<>();
-
-        for(int i=0; i < s.length();i++)
+        
+        for(int i=0;i<s.length();i++)
         {
-            if(s.charAt(i) == '(')
+            char current =  s.charAt(i);
+            if(current == '(')
             {
-                stack.push('(');
+                stack.push(current);
+                continue;
+            }
+            if(stack.isEmpty())
+            {
+                return false;
             }
             else{
-                if(stack.isEmpty())
-                {
-                    return false;
-                }
-                else{
-                    stack.pop();
-                }
+                stack.pop();
             }
+            
         }
-
-        if(!stack.isEmpty())
+        if(stack.isEmpty())
         {
+            return true;
+        }
+        else{
             return false;
         }
 
-
-
-        return answer;
     }
 }
