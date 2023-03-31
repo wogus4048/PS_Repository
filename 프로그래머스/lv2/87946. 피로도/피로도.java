6261560a0ce6f72fd6124659392aaad75a87949d
Gil Jae Hyun <67767276+wogus4048@users.noreply.github.com>
2023-03-31 17:44:45 +0900

    [level 2] Title: 피로도, Time: 0.04 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.java
+++ 프로그래머스/lv2/87946. 피로도/피로도.java
@@ -1,38 +1,35 @@
 class Solution {
-    static int answer = -1;
+    static int answer = 0;
     
     public int solution(int k, int[][] dungeons) {
-
-        for(int i=dungeons.length; i>1 ;i--)
-        {
-            boolean[] visited = new boolean[dungeons.length];
-            permutation(dungeons.length,i,visited,0,k,dungeons);
-            if(answer!= -1)
-            {
-                return answer;
-            }
-        }
         
-        for(int i=0;i<dungeons.length;i++)
-        {
-            if(dungeons[i][0] <= k)
-            {
-                return 1;
-            }
-        }
-        return 0;
+        boolean[] visited = new boolean[dungeons.length];
+        permutation(dungeons.length,visited,0,k,dungeons);
+        return answer;
+        
+        // for(int i=dungeons.length; i>1 ;i--)
+        // {
+        //     boolean[] visited = new boolean[dungeons.length];
+        //     permutation(dungeons.length,i,visited,0,k,dungeons);
+        //     if(answer!= -1)
+        //     {
+        //         return answer;
+        //     }
+        // }
+        
+//         for(int i=0;i<dungeons.length;i++)
+//         {
+//             if(dungeons[i][0] <= k)
+//             {
+//                 return 1;
+//             }
+//         }
+//         return 0;
         
     }
-    
-    
-    
-    void permutation(int n, int r, boolean[] visited, int depth,int k,int[][] dungeons )
+        void permutation(int n, boolean[] visited, int depth,int k,int[][] dungeons )
     {
-        if(depth == r)
-        {
-            answer = r;
-            return;
-        }
+        answer = Math.max(answer,depth);
         
         for(int i=0;i<n;i++)
         {
@@ -45,11 +42,39 @@ class Solution {
                 }
             
                 visited[i] = true;
-                permutation(n,r,visited,depth+1,k-dungeons[i][1],dungeons);
+                permutation(n,visited,depth+1,k-dungeons[i][1],dungeons);
                 visited[i] = false;
             }
         }
     }
+    
+    
+    
+//     void permutation(int n, int r, boolean[] visited, int depth,int k,int[][] dungeons )
+//     {
+        
+//         if(depth == r)
+//         {
+//             answer = r;
+//             return;
+//         }
+        
+//         for(int i=0;i<n;i++)
+//         {
+//             if(!visited[i])
+//             {
+//                 //방문전 갈수있는지 체크 
+//                 if(dungeons[i][0] > k)
+//                 {
+//                     continue;
+//                 }
+            
+//                 visited[i] = true;
+//                 permutation(n,r,visited,depth+1,k-dungeons[i][1],dungeons);
+//                 visited[i] = false;
+//             }
+//         }
+//     }
     
 }
 
