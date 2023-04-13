0586a9b8237b8771b026041102dc1abdec52ed55
Gil Jae Hyun <67767276+wogus4048@users.noreply.github.com>
2023-04-13 13:10:36 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 476 ms, Memory: 16096 KB -BaekjoonHub

--- 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
@@ -1,49 +1,53 @@
 import java.io.*;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.StringTokenizer;
-
-
 public class Main {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-    static int[] fiboStore = new int[41];
-    static int fibo1Count;
-    static int fibo2Count;
+    static int testcase;
+    static int[] dp = new int[41];
+    static int count1 =0;
+    static int count2 =0;
 
     public static void main(String[] args) throws IOException {
-        int n = Integer.parseInt(br.readLine());
+        input();
+        solve();
 
-        fibo1(n);
-        fibo2(n);
-
-        bw.write(fibo1Count + " " + fibo2Count);
         bw.flush();
         bw.close();
 
     }
 
-    public static int fibo1(int n) { //재귀
-        if (n == 1 || n == 2) {
-            fibo1Count++;
+    static void input() throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        fibo1(n);
+        fibo2(n);
+        bw.write(count1 + " " + count2);
+
+
+    }
+    static void solve() throws IOException {
+
+    }
+
+    static int fibo1(int n )
+    {
+        if(n==1 || n==2)
+        {
+            count1++;
             return 1;
         }
         return fibo1(n - 1) + fibo1(n - 2);
     }
 
-    public static int fibo2(int n) {
-        fiboStore[1] = 1;
-        fiboStore[2] = 1;
+    static int fibo2(int n) {
+        dp[1] = 1;
+        dp[2] = 1;
         for (int i = 3; i <= n; i++) {
-            fiboStore[i] = fiboStore[i - 1] + fiboStore[i - 2];
-            fibo2Count++;
+            dp[i] = dp[i-1] + dp[i-2];
+            count2++;
         }
-        return fiboStore[n];
+        return dp[n];
     }
-
 
 }
