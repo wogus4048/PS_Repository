17c6f91a8043b3211964f53c086f93a946d19dc5
Gil Jae Hyun <67767276+wogus4048@users.noreply.github.com>
2023-04-13 14:06:02 +0900

    [Silver II] Title: 연속합, Time: 296 ms, Memory: 26148 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -1,27 +1,14 @@
 import java.io.*;
-
+import java.util.Arrays;
 public class Main {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-
-    /*
-    n개의 수가 주어지고, 그 숫자들중 연속된 몇개 수를 선택해서 최대값을 구한다. 최소 한개 이상 선택해야한다.
-    정수의갯수는 최대 10만개, 10만개를 1개뽑는,2개뽑는...n개뽑는 조합을 모두구해서 최대값을 구하면 무조건 시간초과 조합 -> O(2^n)
-    해당 위치의 숫자와, 그 숫자를 더했을때 어떤값이 더큰지 체크한다, 그렇게함으로서 그 숫자를 선택하지않았을때와, 그 숫자를 선택했을 2경우의 최대값을 체크할 수 있다.
-    각 숫자에 도착했을때 뽑고,안뽑고의 둘중 최대값을 구해보는것이므로 모든경우를 확인한다고 볼 수 있다.(연속된 수를 뽑는 문제니까)
-
-     */
-
+    static Integer[] dp = new Integer[100001];
+    static int[] array;
     static int n;
-    static int[] numbers;
-    static int[] sums;
-    static int max = Integer.MIN_VALUE;
-
-
+    static int max;
     public static void main(String[] args) throws IOException {
-
         input();
         solve();
 
@@ -32,30 +19,22 @@ public class Main {
 
     static void input() throws IOException {
         n = Integer.parseInt(br.readLine());
-        numbers = new int[n];
-        sums = new int[n];
+        array = new int[n + 1];
         String[] input = br.readLine().split(" ");
-        for (int i = 0; i < n; i++) {
-            numbers[i] = Integer.parseInt(input[i]);
+        for (int i = 1; i <= n; i++) {
+            array[i] = Integer.parseInt(input[i - 1]);
         }
-    }
+        max = array[1];
 
+    }
     static void solve() throws IOException {
-        int tempSum = numbers[0];
-        sums[0] = numbers[0];
-        for (int i = 1; i < n; i++) {
-            int tempMax = Math.max(numbers[i], numbers[i] + tempSum); //해당 위치의 숫자와, 그 숫자를 더했을때 어떤값이 더큰지 체크한다.
-            sums[i] = tempMax;
-            tempSum = tempMax;
+        dp[1] = array[1];
+        for (int i = 2; i <= n; i++) {
+            dp[i] = Math.max(dp[i - 1] + array[i], array[i]);
+            max = Math.max(dp[i], max);
         }
-
-        for (int i = 0; i < n; i++) {
-//            System.out.println("sums = " + sums[i]);
-            max = Math.max(max, sums[i]);
-        }
-        bw.write(max+"");
+        bw.write(max + "");
 
     }
-
 
 }
