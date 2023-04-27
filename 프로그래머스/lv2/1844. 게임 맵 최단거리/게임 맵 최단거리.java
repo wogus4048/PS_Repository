8673e297f952f6dd9025d6684e4b921cc5e3a98a
Gil Jae Hyun <67767276+wogus4048@users.noreply.github.com>
2023-04-27 12:50:50 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.04 ms, Memory: 52.8 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,50 +1,72 @@
 import java.util.*;
+
 class Solution {
-    int[] dx = {1, 0, -1, 0};
-    int[] dy = {0, 1, 0, -1};
+    
+    static boolean[][] visited;
+    static int n,m;
+    static int[] di = {1,-1,0,0};
+    static int[] dj = {0,0,1,-1};
+    static int result = 0;
     
     public int solution(int[][] maps) {
-        int answer = 0;
+        /*
+        bfs 최소 이동거리 문제
+        */
+        n = maps.length;
+        m = maps[0].length;
         
-        int[][] visited = new int[maps.length][maps[0].length];
         
-        bfs(maps, visited);
-        answer = visited[maps.length-1][maps[0].length-1];
-        
-        if(answer == 0){
-            answer = -1;
+        visited = new boolean[n][m];
+
+        bfs(maps);
+        if(result == 0)
+        {
+            return -1;
         }
-        
-        return answer;
+        else{
+            return result+1;
+        }
+
     }
     
-    public void bfs(int[][] maps, int[][] visited){
-        int x = 0;
-        int y = 0;
-        visited[x][y] = 1;
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{x, y});
+    static void bfs(int[][] maps)
+    {
+        Queue<int[]> q= new LinkedList<>();
+        q.add(new int[]{0,0,0}); //i , j , step
+        visited[0][0] = true;
         
-        while(!queue.isEmpty()){
-            int[] current = queue.remove();
-            int cX = current[0];
-            int cY = current[1];
+        while(!q.isEmpty())
+        {
+            int[] current = q.poll();
+            int currentI = current[0];
+            int currentJ = current[1];
+            int currentStep = current[2];
             
-            for(int i = 0; i < 4; i++){
-                int nX = cX + dx[i];
-                int nY = cY + dy[i];
-                
-                if(nX < 0 || nX > maps.length-1 || nY < 0 || nY > maps[0].length-1)
-                    continue;
-                
-                if(visited[nX][nY] == 0 && maps[nX][nY] == 1){
-                    visited[nX][nY] = visited[cX][cY] + 1;
-                    queue.add(new int[]{nX, nY});
-                }
+            if(currentI == n-1 && currentJ == m-1)
+            {
+                result = currentStep;
+                break;
             }
+            
+            for(int i=0;i<4;i++)
+            {
+                int nextI = currentI + di[i];
+                int nextJ = currentJ + dj[i];
+                
+                if(nextI >=0 && nextI <n && nextJ >=0 && nextJ <m)
+                {
+                    if(!visited[nextI][nextJ] && maps[nextI][nextJ] == 1)
+                    {
+                        visited[nextI][nextJ]= true;
+                        q.add(new int[]{nextI,nextJ,currentStep+1});
+                    }
+                }
+                
+            }
+            
             
         }
         
-        
+    
     }
 }
