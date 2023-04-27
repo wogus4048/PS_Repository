b07a0c1572049d85ce2b4dff38f0cfe69699bf21
Gil Jae Hyun <67767276+wogus4048@users.noreply.github.com>
2023-04-27 11:11:27 +0900

    [level 2] Title: 타겟 넘버, Time: 0.64 ms, Memory: 70.6 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,24 +1,32 @@
-public class Solution {
-    int answer = 0;
-    public int solution(int[] numbers, int target) {
+import java.util.*;
 
+class Solution {
+    // static boolean[] visited;
+    static int count=0;
 
+    static public int solution(int[] numbers, int target) {
+        // visited = new boolean[numbers.length];
 
-        dfs(numbers,0,target,0);
+        dfs(numbers,target,0,0);
 
-        return answer;
+        System.out.println(count);
+        return count;
     }
 
-    public void dfs(int[] numbers, int depth, int target, int sum){
-        if(depth == numbers.length){ // 마지막 노드까지 탐색한 경우
-            if(target == sum) answer++;
-        } else { // 탐색할 노드가 남아있는 경우
-            dfs(numbers, depth + 1, target, sum + numbers[depth]); // 해당 노드의 값을 더하고 다음 깊이 탐색
-            dfs(numbers, depth + 1, target, sum - numbers[depth]); // 해당 노드의 값을 빼고 다음 깊이 탐색
+    static void dfs(int[] numbers , int target, int sum, int index)
+    {
+        if(index == numbers.length){
+
+            if(sum == target)
+            {
+                count++;
+            }
+            return;
         }
+
+        dfs(numbers,target,sum+numbers[index],index+1);
+        dfs(numbers,target,sum-numbers[index],index+1);
+
+
     }
-
-
-
-
 }
