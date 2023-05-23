0d7f58572ace312e18a5f0eeadbcb78cd8c44624
Gil Jae Hyun <67767276+wogus4048@users.noreply.github.com>
2023-05-23 17:57:46 +0900

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ 프로그래머스/lv4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -1,20 +1,18 @@
--- 코드를 입력하세요
-
-select member_name, review_text, date_format(review_date,'%Y-%m-%d')
-from member_profile mp
+select m.member_name,r.review_text,date_format(r.review_date,'%Y-%m-%d') as review_date
+from member_profile m
 join rest_review r
-on mp.member_id = r.member_id
-where mp.member_id in (
-SELECT member_id
-from REST_REVIEW
+on m.member_id = r.member_id
+where m.member_id in(select member_id
+from rest_review
 group by member_id
-having count(*) = (SELECT count(*)  
-from REST_REVIEW
+having count(*) = (
+ select count(*) 
+from rest_review
 group by member_id
-order by count(*) desc
-limit 1)
-)
-order by review_date,review_text
+order by count(*)  desc
+    limit 1
+))
+order by review_date asc , review_text asc
 
 
 
@@ -24,3 +22,32 @@ order by review_date,review_text
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# select member_name, review_text, date_format(review_date,'%Y-%m-%d')
+# from member_profile mp
+# join rest_review r
+# on mp.member_id = r.member_id
+# where mp.member_id in (
+# SELECT member_id
+# from REST_REVIEW
+# group by member_id
+# having count(*) = (SELECT count(*)  
+# from REST_REVIEW
+# group by member_id
+# order by count(*) desc
+# limit 1)
+# )
+# order by review_date,review_text
