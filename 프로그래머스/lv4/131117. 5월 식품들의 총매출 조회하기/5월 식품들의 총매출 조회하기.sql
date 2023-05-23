d95b151a337a60237715a5a7935200bdb3420952
Gil Jae Hyun <67767276+wogus4048@users.noreply.github.com>
2023-05-23 18:07:49 +0900

    [level 4] Title: 5월 식품들의 총매출 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/131117. 5월 식품들의 총매출 조회하기/5월 식품들의 총매출 조회하기.sql
+++ 프로그래머스/lv4/131117. 5월 식품들의 총매출 조회하기/5월 식품들의 총매출 조회하기.sql
@@ -1,14 +1,32 @@
--- 코드를 입력하세요
-
-select ff.product_id,ff.product_name,(f.total * ff.price) as TOTAL_SALES
-from food_product ff
-join (
-SELECT product_id , sum(amount) as total
-from food_order
-where PRODUCE_DATE like '2022-05%'
-group by product_id
-) as f
-on ff.product_id = f.product_id
-order by TOTAL_SALES desc, ff.product_id
+select fp.product_id,fp.product_name,  sum(fo.amount*fp.price) as total_sales
+from food_product fp
+join food_order fo
+on fp.product_id = fo.product_id
+# where fo.produce_date like '2022-05%'
+where date_format(fo.produce_date,'%Y-%m-%d') between '2022-05-01' and '2022-05-31'
+group by fp.product_id
+order by total_sales desc , fp.product_id asc
 
 
+# 
+
+
+
+
+
+
+
+
+
+
+
+# select ff.product_id,ff.product_name,(f.total * ff.price) as TOTAL_SALES
+# from food_product ff
+# join (
+# SELECT product_id , sum(amount) as total
+# from food_order
+# where PRODUCE_DATE like '2022-05%'
+# group by product_id
+# ) as f
+# on ff.product_id = f.product_id
+# order by TOTAL_SALES desc, ff.product_id
