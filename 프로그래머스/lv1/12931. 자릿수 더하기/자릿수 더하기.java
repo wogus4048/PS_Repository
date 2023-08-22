a2112ce66c51358818076d7526e38593c34677ee
Gil Jae Hyun <67767276+wogus4048@users.noreply.github.com>
2023-08-22 13:17:51 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -2,14 +2,29 @@ import java.util.*;
 
 public class Solution {
     public int solution(int n) {
-        String number = Integer.toString(n);
+        // String number = Integer.toString(n);
+        // int answer =0;
+        // for(int i=0;i<number.length();i++)
+        // {
+        //     answer += number.charAt(i) - '0';
+        // }
+        // System.out.println(answer+"");
+        // return answer;
+        
         int answer =0;
-        for(int i=0;i<number.length();i++)
+        
+        while(true)
         {
-            answer += number.charAt(i) - '0';
+            answer += n % 10;
+            
+            if(n/10 == 0)
+            {
+                return answer;
+            }
+            
+            n = n/10;
         }
-        System.out.println(answer+"");
-        return answer;
+        
         
     }
 }
