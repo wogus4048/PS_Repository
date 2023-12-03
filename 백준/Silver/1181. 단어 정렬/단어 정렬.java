688285417dfb256c408eafe25e890c2b3cf03489
Gil Jae Hyun <67767276+wogus4048@users.noreply.github.com>
2023-12-03 14:26:20 +0900

    [Silver V] Title: 단어 정렬, Time: 380 ms, Memory: 23476 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,43 +1,73 @@
-
 import java.io.*;
 import java.lang.reflect.Array;
-import java.util.*;
-import java.util.Map.Entry;
-public class Main {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
 
+public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+
+    static int n;
+    static Set<String> set = new HashSet<>();
+    static List<String> list ;
+
     public static void main(String[] args) throws IOException {
-
-        int n = Integer.parseInt(br.readLine());
-        Set<String> arraySet = new HashSet<>();
-
-        for (int i = 0; i < n; i++) {
-            arraySet.add(br.readLine());
-        }
-        String[] array = arraySet.toArray(new String[0]);
-
-        Arrays.sort(array, (o1, o2) -> {
-            if (o1.length() == o2.length()) {
-                for (int i = 0; i < o1.length(); i++) {
-                    if (o1.charAt(i) != o2.charAt(i)) {
-                        return o1.charAt(i) - o2.charAt(i);
-                    }
-                }
-                return 0;
-            } else {
-                return o1.length() - o2.length();
-            }
-
-        });
-
-        for (String current : array) {
-            bw.write(current + "\n");
-        }
+        input();
+        solve();
 
         bw.flush();
         bw.close();
 
     }
+
+    static void input() throws IOException {
+
+        n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            set.add(br.readLine());
+        }
+        list = new ArrayList<>(set);
+
+
+
+    }
+    static void solve() throws IOException {
+
+        list.sort((o1,o2) ->
+        {
+            if(o1.length() == o2.length())
+            {
+                return o1.compareTo(o2);
+
+            }else{
+                return o1.length() - o2.length();
+            }
+
+        });
+
+        for (String s : list) {
+            bw.write(s+"\n");
+
+        }
+
+
+
+
+    }
+
+
+
+
+
+
 }
