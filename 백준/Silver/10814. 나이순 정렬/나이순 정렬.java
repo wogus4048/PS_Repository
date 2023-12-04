94f94eee11580af120e68f54c334931cebae75ec
Gil Jae Hyun <67767276+wogus4048@users.noreply.github.com>
2023-12-04 22:01:45 +0900

    [Silver V] Title: 나이순 정렬, Time: 876 ms, Memory: 59044 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,50 +1,79 @@
+
 import java.io.*;
 import java.lang.reflect.Array;
-import java.util.*;
-import java.util.Map.Entry;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
 
 public class Main {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+
+    static int n;
+    static List<Member> list = new ArrayList<>();
+
+
     public static void main(String[] args) throws IOException {
-
-        int n = Integer.parseInt(br.readLine());
-        List<Member> members = new ArrayList<>();
-        for (int i = 1; i <= n; i++) {
-            String[] input = br.readLine().split(" ");
-            members.add(new Member(i, Integer.parseInt(input[0]), input[1]));
-        }
-
-        members.sort((o1,o2) -> {
-            if (o1.age == o2.age) {
-                return o1.memberNumber - o2.memberNumber;
-            }
-            else{
-                return o1.age - o2.age;
-            }
-        });
-
-        for (int i = 0; i < n; i++) {
-            bw.write(members.get(i).age + " " + members.get(i).name+"\n");
-        }
+        input();
+        solve();
 
         bw.flush();
         bw.close();
 
     }
 
-    static class Member {
+    static void input() throws IOException {
 
-        int memberNumber;
+        n = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= n; i++) {
+            String[] input = br.readLine().split(" ");
+            list.add(new Member(i, Integer.parseInt(input[0]), input[1]));
+            //순서 , 나이 ,이름순
+        }
+
+    }
+
+    static void solve() throws IOException {
+
+        list.sort((o1,o2) -> {
+            if (o1.age == o2.age) {
+                return o1.order - o2.order;
+            }
+            else{
+                return o1.age - o2.age;
+            }
+
+
+        });
+
+        for (Member currentM : list) {
+            bw.write(currentM.age + " " + currentM.name+"\n");
+        }
+
+    }
+    static class Member{
         int age;
+        int order;
         String name;
 
-        public Member(int memberNumber, int age, String name) {
-            this.memberNumber = memberNumber;
+        public Member(int order, int age, String name) {
+            this.order = order;
             this.age = age;
             this.name = name;
         }
+
     }
+
+
 }
