e60172d62c49d72b13379987f3b539faca848291
Gil Jae Hyun <67767276+wogus4048@users.noreply.github.com>
2024-01-20 16:08:40 +0900

    [Bronze II] Title: 커트라인, Time: 128 ms, Memory: 14412 KB -BaekjoonHub

--- 백준/Bronze/25305. 커트라인/커트라인.java
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -1,28 +1,67 @@
+
 import java.io.*;
 import java.lang.reflect.Array;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+
+
+
     public static void main(String[] args) throws IOException {
+        input();
+        solve();
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-        List<Integer> scoreList = new ArrayList<>();
-
-        st = new StringTokenizer(br.readLine());
-        while (st.hasMoreTokens()) {
-            scoreList.add(Integer.parseInt(st.nextToken()));
-        }
-
-        scoreList.sort(Comparator.reverseOrder());
-        bw.write(scoreList.get(k - 1)+"\n");
-        
         bw.flush();
         bw.close();
+
     }
+
+    static void input() throws IOException {
+
+
+    }
+
+    static void solve() throws IOException {
+
+        String[] input = br.readLine().split(" ");
+        int n = Integer.parseInt(input[0]);
+        int target = Integer.parseInt(input[1]);
+
+        int[] arr = new int[n];
+
+        input = br.readLine().split(" ");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(input[i]);
+        }
+
+        Arrays.sort(arr);
+        System.out.println(arr[arr.length-target]);
+
+
+
+
+
+    }
+
+
+
+
 }
