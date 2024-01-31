e2397c1f4b88d9da008248278264db449fa09494
Gil Jae Hyun <67767276+wogus4048@users.noreply.github.com>
2024-01-31 19:22:45 +0900

    [Silver V] Title: 영화감독 숌, Time: 288 ms, Memory: 84344 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -1,50 +1,65 @@
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
-import java.util.StringTokenizer;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
-  static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-  static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-  static int n;
-  static String result = "666";
-  static int count = 1;
-  static int num = 666;
 
-  public static void main(String[] args) throws IOException {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    input();
-    solve();
-    // test();
-    bw.write(num + "\n");
+    static int n;
 
-    bw.flush();
-    br.close();
-    bw.close();
-  }
 
-  static void input() throws IOException {
-    n = Integer.parseInt(br.readLine());
 
-  }
 
-  static void solve() throws IOException {
-    while (count != n) {
-      num++;
-      if (Integer.toString(num).contains("666")) {
-        count++;
-      }
+    public static void main(String[] args) throws IOException {
+        input();
+        solve();
+
+        bw.flush();
+        bw.close();
 
     }
 
-  }
+    static void input() throws IOException {
 
-  static void test() throws IOException {
+        n = Integer.parseInt(br.readLine());
+        if (n == 1) {
+            bw.write(666 + "");
+            return;
+        }
 
-  }
+    }
+
+    static void solve() throws IOException {
+        int count =1;
+        int num = 666;
+        while (count < n) {
+            num++;
+            if (Integer.toString(num).contains("666")) {
+                count++;
+                if (count == n) {
+                    bw.write(num+"");
+                    break;
+                }
+            }
+
+        }
+    }
+
+
+
 }
