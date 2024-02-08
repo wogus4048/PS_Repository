1ed6640a549dfe17defa5501c249858f1654baba
Gil Jae Hyun <67767276+wogus4048@users.noreply.github.com>
2024-02-08 15:31:57 +0900

    [Bronze I] Title: 약수, Time: 124 ms, Memory: 14340 KB -BaekjoonHub

--- 백준/Bronze/1037. 약수/약수.java
+++ 백준/Bronze/1037. 약수/약수.java
@@ -1,7 +1,19 @@
 import java.io.*;
 import java.lang.reflect.Array;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
 
@@ -9,24 +21,38 @@ public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 
-    public static void main(String[] args) throws IOException {
-        int n = Integer.parseInt(br.readLine());
-        List<Integer> numberList = new ArrayList<>();
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            numberList.add(Integer.parseInt(st.nextToken()));
-        }
 
-        if (numberList.size() == 1) {
-            bw.write((int)Math.pow(numberList.get(0), 2)+"");
-        }
-        else{
-            numberList.sort(Comparator.naturalOrder());
-            bw.write(numberList.get(0)*numberList.get(numberList.size()-1)+"");
-        }
+    public static void main(String[] args) throws IOException {
+        input();
+        solve();
 
         bw.flush();
         bw.close();
+
+    }
+
+    static void input() throws IOException {
+
+
+    }
+
+    static void solve() throws IOException {
+
+        int n = Integer.parseInt(br.readLine());
+        String[] input = br.readLine().split(" ");
+        int[] ar = new int[n];
+        for (int i = 0; i < n; i++) {
+            ar[i] = Integer.parseInt(input[i]);
+        }
+
+        Arrays.sort(ar);
+
+        System.out.println(ar[0] * ar[n - 1]);
+
+
+
+
+
 
     }
 
