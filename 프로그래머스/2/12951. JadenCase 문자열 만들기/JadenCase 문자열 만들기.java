e4c692989f88b47df8a1d13f5d64e4b05bac8e91
Gil Jae Hyun <67767276+wogus4048@users.noreply.github.com>
2024-03-05 15:35:17 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 13.19 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,35 +1,20 @@
-import java.util.*;
+
 class Solution {
     public String solution(String s) {
-        //받아서 split, 각 배열요소 첫번쨰는 대문자로 바꾸고 나머지는 소문자처리 
-        
-        String[] input = s.split(" ");
-        StringBuilder sb = new StringBuilder();
-        
-        if(input.length == 0)
+        String answer = "";
+
+        String[] str = s.split(" ");
+
+        for(String ss : str)
         {
-            return s;
+            if(ss.length() > 0)
+                answer += ss.substring(0,1).toUpperCase() + ss.substring(1).toLowerCase() + " ";
+            else
+                answer += " ";
         }
-        
-        for(int i=0;i<input.length;i++)
-        {
-            String word = input[i];
-            if(word.length() == 0)
-            {
-                sb.append(" ");
-                continue;
-            }
-            else{
-                String tempResult = 
-                word.substring(0,1).toUpperCase() + word.substring(1,word.length()).toLowerCase();
-            sb.append(tempResult).append(" ");
-            }
-            
-        }
-        if(s.charAt(s.length()-1) == ' ')
-        {
-            return sb.substring(0,sb.length());
-        }
-        return sb.substring(0,sb.length()-1);
+
+        if (s.charAt(s.length() - 1) != ' ')
+            answer = answer.substring(0, answer.length()-1);
+        return answer;
     }
 }
--- 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] JadenCase 문자열 만들기 - 12951 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951#) 
 
 ### 성능 요약
 
-메모리: 72.8 MB, 시간: 1.46 ms
+메모리: 74.9 MB, 시간: 13.19 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 05일 15:13:29
+2024년 03월 05일 15:35:15
 
 ### 문제 설명
 
