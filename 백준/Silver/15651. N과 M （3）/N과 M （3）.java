dcbeb2f44af8c11a814e6cd94daec91647a978dc
Gil Jae Hyun <67767276+wogus4048@users.noreply.github.com>
2024-03-05 12:29:53 +0900

    [Silver III] Title: N과 M (3), Time: 556 ms, Memory: 158616 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,38 +1,77 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    /**
-     * 중복 순열
-     * 중복을 막는 방문배열을 없앤다.
-     */
+    static int n,m;
 
     public static void main(String[] args) throws IOException {
-        String[] input = br.readLine().split(" ");
-        int n = Integer.parseInt(input[0]);
-        int r = Integer.parseInt(input[1]);
-        int[] result = new int[r];
-        permutation(n, r,  result, 0);
+        input();
+        solve();
+
         bw.flush();
         bw.close();
+    }
+
+    static void input() throws IOException {
+        String[] input = br.readLine().split(" ");
+        n = Integer.parseInt(input[0]);
+        m = Integer.parseInt(input[1]);
+
+//        combi(n, m, 0, 0, new boolean[n]);
+        permu(n,m, 0, new int[m]);
 
     }
 
-    public static void permutation(int n, int r,int[] result,int count) throws  IOException{
-        if (count == r) {
-            for (int i = 0; i < r; i++) {
-                bw.write(result[i] + " ");
+    static void solve() throws IOException {
+
+
+
+
+    }
+
+    static void permu(int n, int r, int depth,int[] result) throws IOException {
+        if (r == depth) {
+            for (int i : result) {
+                bw.write(i+" ");
             }
             bw.newLine();
-            return;
+            return; 
         }
+
         for (int i = 0; i < n; i++) {
-            result[count] = i + 1; //결과값을 저장할때 인덱스는 지금까지 뽑은갯수여야만 한다.
-            permutation(n, r,result,count+1);
+            result[depth] = i+1;
+            permu(n, r, depth + 1, result);
+        }
+    }
+
+
+    static void combi(int n, int r, int depth, int index, boolean[] visited) {
+        if (depth == r) {
+            for (int i = 0; i < n; i++) {
+                if (visited[i]) {
+                    System.out.print(i + 1+" ");
+                }
+            }
+            System.out.println();
+        }
+
+        for (int i = index; i < n; i++) {
+            visited[i] = true;
+            combi(n, r, depth + 1, i + 1, visited);
+            visited[i] = false;
         }
 
     }
+
 
 }
