3a375becbdd6246f6c0dd77b7a9330f7a87e9932
Gil Jae Hyun <67767276+wogus4048@users.noreply.github.com>
2024-03-15 10:15:19 +0900

    [level 2] Title: 구명보트, Time: 9.32 ms, Memory: 53.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        int length = people.length;
+        Arrays.sort(people);
+        int index=0;
+        
+        for(int i=length-1;i>=index;i--)
+        {
+            if(people[i] + people[index] <= limit)
+            {
+                answer++;
+                index++;
+            }
+            else{
+                answer++;            
+            }
+        }
+        
+        
+        
+        return answer;
+    }
+}
