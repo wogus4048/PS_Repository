87eae8c39155c922bd7eae6728edd170bad2ab98
Gil Jae Hyun <67767276+wogus4048@users.noreply.github.com>
2024-03-24 19:24:42 +0900

    [level 1] Title: 역순 정렬하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/59035. 역순 정렬하기/역순 정렬하기.sql
@@ -0,0 +1,7 @@
+-- 코드를 입력하세요
+# SELECT name,Date_format(datetime,'%Y-%m-%d %H:%i:%s') as DATETIME
+# from ANIMAL_INS 
+# order by animal_id desc
+SELECT name,datetime
+from ANIMAL_INS 
+order by animal_id desc
