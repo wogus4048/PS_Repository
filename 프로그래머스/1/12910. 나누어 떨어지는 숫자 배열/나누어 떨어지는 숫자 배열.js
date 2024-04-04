aad66fa1b7f7141b2aa934077069aab3faefd06c
Gil Jae Hyun <67767276+wogus4048@users.noreply.github.com>
2024-04-04 15:43:37 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.js
@@ -0,0 +1,8 @@
+function solution(arr, divisor) {
+    var answer = [];
+    
+    answer = arr.filter((v) => v%divisor==0).sort((a,b) => a-b);
+    
+    
+    return answer.length!=0 ? answer : [-1];
+}
