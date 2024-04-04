d92f7a266cb57795b7851a5977b927c0bb7e46ee
Gil Jae Hyun <67767276+wogus4048@users.noreply.github.com>
2024-04-04 15:54:45 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 2.80 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/README.md
+++ 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/README.md
@@ -1,10 +1,10 @@
 # [level 1] 나누어 떨어지는 숫자 배열 - 12910 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12910?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12910?language=java) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.09 ms
+메모리: 76.6 MB, 시간: 2.80 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 04일 15:43:36
+2024년 04월 04일 15:54:44
 
 ### 문제 설명
 
--- 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.java
+++ 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.java
@@ -1,21 +1,24 @@
 import java.util.*;
 class Solution {
-    public List<Integer> solution(int[] arr, int divisor) {
+    public int[] solution(int[] arr, int divisor) {
         
-        List<Integer> list =  new ArrayList<>();
+//         List<Integer> list =  new ArrayList<>();
         
-        for(int num : arr)
-        {
-            if(num % divisor ==0) list.add(num);
-        }
-        if(list.size() ==0) 
-        {
-            list.add(-1);
-            return list;
-        }
-        list.sort(Comparator.naturalOrder());
+//         for(int num : arr)
+//         {
+//             if(num % divisor ==0) list.add(num);
+//         }
+//         if(list.size() ==0) 
+//         {
+//             list.add(-1);
+//             return list;
+//         }
+//         list.sort(Comparator.naturalOrder());
         
         
-        return list;
+//         return list;
+        
+        int[] result = Arrays.stream(arr).filter((v) -> v%divisor == 0).sorted().toArray();
+        return result.length!=0 ? result : new int[]{-1};
     }
 }
